94e20b84751a9f06607e661401afc6d538e482bd
Sunny Chaware <47352505+SunnyChaware@users.noreply.github.com>
2020-06-02 12:44:25 +0530

    Add files via upload

---
+++ divisible-sum-pairs.cpp
@@ -0,0 +1,36 @@
+/*
+Author = Sunny chaware
+Made on = 02 June 2020
+*/
+
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int arr[100], n , i , k , j ;
+    int count = 0;
+
+    // taking the values of n and k
+    // n is the integer length of array
+    // k is the integer for division
+    // cout<<"Enter n and k"<<endl;
+    cin>> n >> k ;
+
+    // taking array as an input
+    for (i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if((arr[i] + arr[j]) % k == 0){
+                count += 1;
+            }
+        }
+    }
+    cout<<count;
+}
